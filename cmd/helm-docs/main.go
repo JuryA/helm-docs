f4ab0dee7ee70ef6731e5142781c396315996ca8
Jiří Altman <11990780+JuryA@users.noreply.github.com>
2025-07-11 20:11:28 +0200

    Improve schema generation

--- cmd/helm-docs/main.go
+++ cmd/helm-docs/main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
+	"github.com/norwoodj/helm-docs/pkg/cueutil"
 	"github.com/norwoodj/helm-docs/pkg/document"
 	"github.com/norwoodj/helm-docs/pkg/helm"
 )
@@ -165,6 +166,38 @@ func writeDocumentation(chartSearchRoot string, documentationInfoByChartPath map
 		}
 		document.PrintDocumentation(info, chartSearchRoot, templateFiles, dryRun, version, badgeStyle, dependencyValues, skipVersionFooter)
 	})
+}
+
+func runSchema(_ *cobra.Command, _ []string) error {
+	initializeCli()
+
+	chartSearchRoot := viper.GetString("chart-search-root")
+	outputRel := viper.GetString("schema-output-file")
+
+	documentationInfoByChartPath, err := readDocumentationInfoByChartPath(chartSearchRoot, 1)
+	if err != nil {
+		return err
+	}
+
+	var failed []string
+	for _, info := range documentationInfoByChartPath {
+		data, err := cueutil.GenerateJSONSchemaFromYAML(info.ChartValues)
+		if err != nil {
+			failed = append(failed, info.ChartDirectory)
+			log.Warnf("schema generation failed for %s: %v", info.ChartDirectory, err)
+			continue
+		}
+		outPath := filepath.Join(chartSearchRoot, info.ChartDirectory, outputRel)
+		if err := os.WriteFile(outPath, data, 0644); err != nil {
+			failed = append(failed, info.ChartDirectory)
+			log.Warnf("failed writing schema for %s: %v", info.ChartDirectory, err)
+		}
+	}
+
+	if len(failed) > 0 {
+		return fmt.Errorf("failed generating schema for charts: %s", strings.Join(failed, ", "))
+	}
+	return nil
 }
 
 func helmDocs(_ *cobra.Command, _ []string) {
---
+++ pkg/cueutil/schema.go
@@ -0,0 +1,56 @@
+package cueutil
+
+import (
+	"bytes"
+	"fmt"
+
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/encoding/jsonschema"
+	cueyaml "cuelang.org/go/encoding/yaml"
+	yamlv3 "gopkg.in/yaml.v3"
+)
+
+// GenerateJSONSchemaFromYAML is a proof-of-concept placeholder that converts a
+// YAML node to a CUE instance. Full JSON Schema generation is left as a future
+// improvement.
+func GenerateJSONSchemaFromYAML(node *yamlv3.Node) ([]byte, error) {
+	if node == nil {
+		return nil, fmt.Errorf("nil node")
+	}
+
+	var buf bytes.Buffer
+	enc := yamlv3.NewEncoder(&buf)
+	if err := enc.Encode(node); err != nil {
+		return nil, err
+	}
+	if err := enc.Close(); err != nil {
+		return nil, err
+	}
+
+	ctx := cuecontext.New()
+	astFile, err := cueyaml.Extract("values.yaml", buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	v := ctx.BuildFile(astFile)
+	if err := v.Err(); err != nil {
+		return nil, err
+	}
+
+	schemaAST, err := jsonschema.Extract(v, &jsonschema.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	schemaVal := ctx.BuildFile(schemaAST)
+	if err := schemaVal.Err(); err != nil {
+		return nil, err
+	}
+
+	out, err := schemaVal.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
